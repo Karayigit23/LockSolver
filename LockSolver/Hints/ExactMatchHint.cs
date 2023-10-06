59f12d42c14a356dd189eeeb69a3700ec825ca34
Karayigit23 <karayigit833@gmail.com>
2023-10-06 11:20:05 +0300

    ExactMatchHint.cs added

--- LockSolver/Hints/ExactMatchHint.cs
+++ LockSolver/Hints/ExactMatchHint.cs
@@ -1,6 +1,15 @@
 namespace LockSolver.Hints;
 
-public class ExactMatchHint
+public class ExactMatchHint:IHint
 {
-    
+    private readonly string _hint;
+
+    public ExactMatchHint(string hint)
+    {
+        _hint = hint;
+    }
+    public bool IsMatch(string candidate)
+    {
+        return candidate.Zip(_hint, (c, h) => c == h ? 1 : 0).Sum() == 1;
+    }
 }
