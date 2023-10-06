565359102afef8a163dde5be934837fd638381f7
Karayigit23 <karayigit833@gmail.com>
2023-10-06 11:32:14 +0300

    RightDigitWrongPlace.cs added

--- LockSolver/Hints/RightDigitWrongPlace.cs
+++ LockSolver/Hints/RightDigitWrongPlace.cs
@@ -1,6 +1,15 @@
 namespace LockSolver.Hints;
 
-public class RightDigitWrongPlace
+public class RightDigitWrongPlace:IHint
 {
-    
+    private readonly string _hint;
+
+    public RightDigitWrongPlace(string hint)
+    {
+        _hint = hint;
+    }
+    public bool IsMatch(string candidate)
+    {
+        return candidate.Zip(_hint, (c, h) => c != h && _hint.Contains(c) ? 1 : 0).Sum() == 0;
+    }
 }
