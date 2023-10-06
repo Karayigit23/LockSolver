7bac5533749e4697b8415cbae4925ec26b599e4b
Karayigit23 <karayigit833@gmail.com>
2023-10-06 11:24:25 +0300

    NoMatchHint.cs added

--- LockSolver/Hints/NoMatchHint.cs
+++ LockSolver/Hints/NoMatchHint.cs
@@ -1,6 +1,16 @@
 namespace LockSolver.Hints;
 
-public class NoMatchHint
+public class NoMatchHint:IHint
 {
+    private readonly string _hint;
+
+    public NoMatchHint(string hint)
+    {
+        _hint = hint;
+    }
     
+    public bool IsMatch(string candidate)
+    {
+        return candidate.Any(c => _hint.Contains(c));
+    }
 }
