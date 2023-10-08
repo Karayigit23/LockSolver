f2a393abdd7ceb56fedf98569106fca7e853746d
Karayigit23 <karayigit833@gmail.com>
2023-10-08 15:25:17 +0300

    created programcs

--- LockSolver/Program.cs
+++ LockSolver/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using LockSolver;
+using LockSolver.Hints;
+
+List<IHint> GetHints_1()
+{
+    return new List<IHint>()
+    {
+        new ExactMatchHint("682"),
+        new RightDigitWrongPlace("614"),
+        new TwoRightDigitWrongPlace("206"),
+        new NoMatchHint("738"),
+        new RightDigitWrongPlace("780")
+
+    };
+}
