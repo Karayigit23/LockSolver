8154081540078d1ebaf4becc63e591b3c5b7a4f5
Karayigit23 <karayigit833@gmail.com>
2023-10-08 15:21:22 +0300

    TwoRightDigitWrongPlace.cs added

--- LockSolver/Hints/TwoRightDigitWrongPlace.cs
+++ LockSolver/Hints/TwoRightDigitWrongPlace.cs
@@ -1,6 +1,15 @@
 namespace LockSolver.Hints;
 
-public class TwoRightDigitWrongPlace
+public class TwoRightDigitWrongPlace:IHint
 {
-    
+    private readonly string _hint;
+
+    public TwoRightDigitWrongPlace(string hint)
+    {
+        _hint = hint;
+    }
+    public bool IsMatch(string candidate)
+    {
+        return candidate.Zip(_hint, (c, h) => c != h && _hint.Contains(c) ? 1 : 0).Sum() == 2;
+    }
 }
