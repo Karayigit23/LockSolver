a4b637a7311cac25c2f44275a62912240169139e
Karayigit23 <karayigit833@gmail.com>
2023-10-08 15:35:29 +0300

    LockSolverClass.cs added

--- LockSolver/LockSolverClass.cs
+++ LockSolver/LockSolverClass.cs
@@ -2,5 +2,29 @@ namespace LockSolver;
 
 public class LockSolverClass
 {
+   
+
+    private readonly List<IHint> hints; 
+    private readonly int digitLength;
+    public LockSolverClass(List<IHint> hints, int digitLength)
+    {
+        this.hints = hints;
+        this.digitLength = digitLength;
+    }
+
+    public string Solve()
+    {
+        for (int i = 0; i < Math.Pow(10, digitLength) - 1; i++)
+        {
+            string candidate = i.ToString("D" + digitLength);
+            if (hints.All(h=>h.IsMatch(candidate)))
+            {
+                return candidate;
+            }
+        }
+
+        return "No Key Found";
+    }
     
+ 
 }
